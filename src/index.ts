9db75d27225778a7e0c1d9f275093b9fd88b323b
motiya <54768914+mtiyz@users.noreply.github.com>
2020-04-25 13:53:40 +0900

    :wrench: add Prettier

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ console.log('site-box')
 
 const currentTabURL = (): string | void => {
   // eslint-disable-next-line no-undef
-  chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
+  chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
     const url = tabs[0].url
     return url
   })
