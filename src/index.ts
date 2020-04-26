2495a21556bf244be241d287bb9cd9e9643353b2
motiya <54768914+mtiyz@users.noreply.github.com>
2020-04-26 16:42:24 +0900

    :sparkles: add toDomain function

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-console.log('site-box')
+// settings
+const loopMinute = 5
 
 const getCurrentTabURL = (): string | void => {
   // eslint-disable-next-line no-undef
@@ -8,4 +9,28 @@ const getCurrentTabURL = (): string | void => {
   })
 }
 
-getCurrentTabURL()
+const toDomain = (url: string): string | void => {
+  return url.match(/^https?:\/{2,}(.*?)(?:\/|\?|#|$)/)?.[1]
+}
+
+const initDomain = (): string | void => {
+  const url = getCurrentTabURL()
+  if (url) {
+    return toDomain(url)
+  }
+}
+
+let currentTabDomain = initDomain()
+
+setInterval(() => {
+  const url = getCurrentTabURL()
+  if (typeof url === 'string') {
+    const domain = toDomain(url)
+    if (domain === currentTabDomain) {
+      // TODO: 時間を増やす
+    } else {
+      currentTabDomain = domain
+      // TODO: 時間を増やす
+    }
+  }
+}, loopMinute * 60000)
